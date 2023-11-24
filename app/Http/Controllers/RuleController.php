aeaab977fa1335f09ab44a3cdb7713756cfd2e22
Meherin <md.shoriful@Mds-MacBook-Pro-2.local>
2023-11-25 01:03:19 +0600

    initial commit

---
+++ app/Http/Controllers/RuleController.php
@@ -0,0 +1,129 @@
+<?php
+// RuleController.php
+
+namespace App\Http\Controllers;
+
+use App\Models\AlertInformation;
+use App\Models\Rule;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RuleController extends Controller
+{
+    // Fetch rules for the authenticated user
+    public function index()
+    {
+        $rules = Rule::where('user_id', auth()->id())->get();
+        $alertInfo = AlertInformation::where('user_id', auth()->id())->first();
+        $data = [];
+        $data['rules'] = $rules;
+        $data['unique_id'] = 'UNIQUE_' . auth()->id();
+        $data['alertInfo'] = $alertInfo;
+        return response()->json($data);
+    }
+
+    // Store a new rule
+    public function store(Request $request)
+    {
+
+        // $validator = Validator::make($request->all(), [
+        //     'rules' => 'required|array',
+        //     'rules.*.show' => 'required|in:Show,Don\'t Show',
+        //     'rules.*.condition' => 'required|in:Contains,Starts With,Ends With,Exact',
+        //     'rules.*.value' => 'required|string',
+        // ]);
+
+        // if ($validator->fails()) {
+        //     return response()->json(['errors' => $validator->errors()], 422);
+        // }
+
+        $rulesData = $request->input('rules');
+        $alertText = $request->input('alertText');
+        if ($alertText) {
+            AlertInformation::updateOrCreate(
+                ['user_id' => auth()->id()],
+                ['text' => $alertText]
+            );
+        }
+
+        Rule::where('user_id', auth()->id())->delete();
+
+        // Insert each rule into the database
+        foreach ($rulesData as $data) {
+            $rule = new Rule();
+            $rule->user_id = auth()->id();
+            $rule->show = $data['show'];
+            $rule->type = $data['type'];
+            $rule->value = $data['value'];
+            $rule->save();
+        }
+
+        return response()->json(['message' => 'Rules inserted successfully']);
+    }
+
+
+    public function generateJsSnippet(Request $request)
+    {
+        $string = $_SERVER['REQUEST_URI'];
+        // Extract the number from the string using regex
+        preg_match('/(\d+)$/', $string, $matches);
+        if (isset($matches[1])) {
+            $userId = (int)$matches[1];
+        } else {
+            $userId = null;
+        }
+
+        $rules = Rule::where('user_id', $userId)->get();
+        $alertText = AlertInformation::where('user_id', $userId)->value('text');
+
+        foreach ($rules as $rule) {
+            // Process each rule condition and construct the JS snippet logic
+            $condition = '';
+
+            switch ($rule['type']) {
+                case 'contains':
+                    $condition = "window.location.href.indexOf('{$rule['value']}') !== -1";
+                    break;
+                case 'start_with':
+                    $condition = "window.location.href.startsWith('{$rule['value']}')";
+                    break;
+                case 'ends_with':
+                    $condition = "window.location.href.endsWith('{$rule['value']}')";
+                    break;
+                case 'specific_page':
+                    $condition = "window.location.href === '{$rule['value']}'";
+                    break;
+                case 'exact':
+                    $condition = "window.location.href === '{$rule['value']}'";
+                    break;
+                default:
+                    $condition =  false;
+                    break;
+            }
+
+            if ($rule['show'] === 'hide') {
+                $dontShowConditions[] = $condition;
+            } else {
+                $showConditions[] = $condition;
+            }
+        }
+
+        // Combine conditions for 'Show' and 'Don't Show' using logical OR (||)
+        $showCondition = !empty($showConditions) ? '(' . implode(' || ', $showConditions) . ')' : 'false';
+        $dontShowCondition = !empty($dontShowConditions) ? '(' . implode(' || ', $dontShowConditions) . ')' : 'false';
+
+        // Construct the JavaScript snippet with the final combined condition
+        $jsSnippet = "if ({$showCondition} && !({$dontShowCondition})) {";
+        $jsSnippet .= "alert('$alertText!');";
+        $jsSnippet .= "}";
+
+        return response($jsSnippet)->header('Content-Type', 'text/javascript');
+    }
+
+    // Delete a rule
+    public function destroy($id)
+    {
+        Rule::where('user_id', auth()->id())->where('id', $id)->delete();
+        return response()->json(['message' => 'Rule deleted successfully']);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RuleController;
+
+
+
+
+Route::middleware('guest')->group(function () {
+    Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup');
+    Route::post('/signup', [AuthController::class, 'signup'])->name('signup.post');
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+});
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard.dashboard');
+    })->name('dashboard');
+
+    Route::get('/', function () {
+        return redirect()->route('dashboard');
+    });
+
+    Route::get('/rules', [RuleController::class, 'index'])->name('rules.index');
+    Route::delete('/rules/{roleId}', [RuleController::class, 'destroy'])->name('rules.destroy');
+    Route::post('/rules/store', [RuleController::class, 'store'])->name('rules.post');
+});
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/generate-script/{uniqueKey}', [RuleController::class, 'generateJsSnippet'])->name('generateJsSnippet');
