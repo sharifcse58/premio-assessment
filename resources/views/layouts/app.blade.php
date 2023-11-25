bebb876db6885c79c65822643894e96cd115d799
Meherin <md.shoriful@Mds-MacBook-Pro-2.local>
2023-11-25 08:22:59 +0600

    User name showed in nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,6 @@
 <html>
 
 <head>
-    <!-- Other head content -->
-
-    <!-- Include Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     @stack('custom_css')
 </head>
@@ -18,7 +15,7 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        User
+                        {{ Auth::user()->name }}
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
@@ -37,12 +34,10 @@
     <!-- Include Vue.js -->
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
 
-    <!-- Include other scripts or assets -->
     <!-- Bootstrap JS CDN -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
     @stack('scripts')
 </body>
 
